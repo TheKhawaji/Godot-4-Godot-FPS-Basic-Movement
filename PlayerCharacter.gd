80bc98b25b55c4bee96360dab2cf773cbd7b07e4
TheFARIS <145927460+TheKhawaji@users.noreply.github.com>
2024-02-27 14:10:40 +0300

    Update PlayerCharacter.gd

--- PlayerCharacter.gd
+++ PlayerCharacter.gd
@@ -5,35 +5,30 @@ extends CharacterBody3D
 @onready var PlayerCollision = $PlayerCollision
 @onready var PlayerShapeCast = $PlayerShapeCast
 
+# Mouse Sensitivity
+@export var _MouseSens : float = deg_to_rad(2.85)
 
-# Palyer Height
-var _CrouchHeight = 1.25
-var _NormalHeight 
-
-#the MAX amount for the player Head/Camera to look either Up or Down most games have this to not let the Head/Camera to loop around the character
+@export_subgroup("Max and Minimum Angles")
+# Max and Minimum Angles for the camera
 @export var _LookUp := deg_to_rad(89)
 @export var _LookDown := deg_to_rad(-80)
 
-@export var _MouseSens : float = deg_to_rad(2.85)
+@export_subgroup("Speeds")
+# Speeds
+var _Speed : float
+@export var _DefaultMovementSpeed : float = 8
+@export var _CrouchMovementSpeed : float = 4
+@export var _AccelerationSpeed : float = 16
+@export var _TransiyionSpeed : float = 12
 
-# PlayerStates
+@export_subgroup("Heights")
+# Palyer Height
+@export var _CrouchHeight : float = 1.25
+@export var _NormalHeight : float
 
-
-
-# Speed 
-var _Speed
-
-# this is the Movement Speed for the player
-@export var _DefaultMovementSpeed = 8
-@export var _CrouchMovementSpeed = 4
-
-
-# the is the Lerp/Acceleration Speed you can remove this if you'd like
-@export var _AccelerationSpeed = 16
-@export var _TransiyionSpeed = 12
-
-@export var _JumpVelocity = 6
-@export var _Gravity = 12
+@export_subgroup("GravityStuff")
+@export var _JumpVelocity : float = 6
+@export var _Gravity : float = 12
 
 # allows the player to move in 3D space
 var _Direction = Vector3.ZERO
@@ -56,6 +51,7 @@ func _unhandled_input(event):
 			Head.rotate_x (deg_to_rad(-event.relative.y * _MouseSens))
 			Head.rotation.x = clamp(Head.rotation.x,_LookDown,_LookUp)
 
+# Handles Inputs
 func _GetInput(delta):
 	
 	# Handle jump.
@@ -68,7 +64,6 @@ func _GetInput(delta):
 		_unCrouch(delta)
 		_Speed = _DefaultMovementSpeed
 
-
 	# Handles Movement
 	_InputDirection = Input.get_vector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward")
 	_Direction = lerp(_Direction,(transform.basis * Vector3(_InputDirection.x, 0, _InputDirection.y)).normalized(), delta * _AccelerationSpeed)
@@ -79,6 +74,7 @@ func _GetInput(delta):
 		velocity.x = 0
 		velocity.z = 0
 
+# this is the Crouch Code
 func _Crouch(delta):
 	PlayerCollision.shape.height = lerp(PlayerCollision.shape.height,_CrouchHeight, _TransiyionSpeed * delta)
 	Head.position.y = lerp(Head.position.y,1.2,_TransiyionSpeed * delta)
